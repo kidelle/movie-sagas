73b9ec79bea4016b55cfff4990e76a42d77b3835
Danielle Martain <dmartain@gmail.com>
2019-07-26 17:11:01 -0500

    initial commit, set up database and components

---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// Set up routes
+import MovieList from '../MovieList/MovieList';
+import Details from '../Details/Details';
+import Edit from '../Edit/Edit';
+
+class App extends Component {
+
+  
+  // Renders the entire app on the DOM
+  render() {
+    return (
+      <div className="App">
+        <p>Empty Page</p>
+      </div>
+    );
+  }
+}
+
+export default App;
